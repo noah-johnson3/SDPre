b532fcb3eed36939294f5d9554db1fcf97acc374
noah-johnson3 <noah.johnson3@yahoo.com>
2022-02-20 14:18:50 -0500

    complete Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,7 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1 =27, num2 = 14, num3 = 8, average = (num1 + num2 + num3) /3;
+    average = (num1 + num2 + num3) /3;
+    System.out.println(average);
+  }
+}
