3c0dea4b81d43efffe6909e5a542709a2e46008b
noah-johnson3 <noah.johnson3@yahoo.com>
2022-02-20 10:40:28 -0500

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+      String name = "Noah Johnson";
+      int age = 26;
+      String state = "New Jersey";
+      System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
