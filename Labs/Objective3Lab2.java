1c3509525ea918154b00a985524ac5177844c1c2
noah-johnson3 <noah.johnson3@yahoo.com>
2022-02-21 09:14:46 -0500

    completed objectives

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,8 @@
+public class Objective3Lab2 {
+  public static void main(String [] args){
+    double side1 = 12, side2 = 8, hypotenuse =  (Math.sqrt ((side1 * side1)+ (side2 * side2)));
+    System.out.println(hypotenuse);
+
+    // 12 * 12 + 8 * 8 = answer
+  }
+}
