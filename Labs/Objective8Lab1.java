77a0582e2d14f8ba23ac3c9e4a7d6b061669e60f
noah-johnson3 <noah.johnson3@yahoo.com>
2022-02-24 11:33:04 -0500

    complete Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,12 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    for (int num = 1; num <= 10; num ++){
+      System.out.println(num);
+      }
+
+    for (int num= 10; num >=1; num--){
+      System.out.println(num);
+      }
+    }
+}
